12e0acb4550c32a648d812cf5ac765dec924a51c
Timothy Roy <timothy.roy@protonmail.ch>
2023-02-10 22:00:03 -0600

    Change to JSON

--- api/index.py
+++ api/index.py
@@ -1,10 +1,15 @@
 from http.server import BaseHTTPRequestHandler
+import json
 
 class handler(BaseHTTPRequestHandler):
 
     def do_GET(self):
         self.send_response(200)
-        self.send_header('Content-type','text/plain')
+        self.send_header('Content-type','json/application')
         self.end_headers()
-        self.wfile.write('Hello, world!'.encode('utf-8'))
+        response = {
+            "message": "Hello, World!",
+            "location": "AWS Lambda"
+        }
+        self.wfile.write(bytes(json.dumps(response), "utf8"))
         return
